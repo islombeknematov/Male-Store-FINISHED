d991a81d380147962a9f1b2116db29de231f74b7
islombeknematov <islombekn159@gmail.com>
2021-12-06 23:48:33 +0500

    last commit

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from api.views import CategoryListAPIView, ProductListAPIView
+from api.views import CategoryListAPIView, ProductListAPIView, ProductRetrieveAPIView
 
 urlpatterns = [
     path('categories/', CategoryListAPIView.as_view()),
     path('products/', ProductListAPIView.as_view()),
+
+    path('products/<int:pk>/', ProductRetrieveAPIView.as_view()),
 ]
+
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from api.serializers import CategoryModelSerializer, ProductModelSerializer
 from products.models import CategoryModel, ProductModel
@@ -12,4 +12,9 @@ class CategoryListAPIView(ListAPIView):
 class ProductListAPIView(ListAPIView):
     queryset = ProductModel.objects.all()
     serializer_class = ProductModelSerializer
+
+
+class ProductRetrieveAPIView(RetrieveAPIView):  # DetailAPIView ==> Rest ==> RetrieveAPIView
+    serializer_class = ProductModelSerializer
+    queryset = ProductModel.objects.all()
 
